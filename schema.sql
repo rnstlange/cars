b4623ddd78ce639b85d414f7c3d027a83999ad6a
Ernest Lange <rnstlange@gmail.com>
2019-06-07 06:34:35 +0300

    Some minor changes in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,15 +1,17 @@
-CREATE TABLE IF NOT EXISTS owners
+CREATE DATABASE IF NOT EXISTS cars;
+
+CREATE TABLE IF NOT EXISTS cars.owners
 (
     id        INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     full_name VARCHAR(50) UNIQUE             NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS cars
+CREATE TABLE IF NOT EXISTS cars.cars
 (
     id    INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     owner INT,
     brand VARCHAR(50)                    NOT NULL,
     model VARCHAR(50)                    NOT NULL,
     year  YEAR                           NOT NULL,
-    FOREIGN KEY (owner) REFERENCES owners (id)
+    FOREIGN KEY (owner) REFERENCES cars.owners (id)
 );
